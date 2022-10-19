9b95526c850e08b3056ae949f6279013e193ebea
ChangmooMoon <public.ellerymoon@gmail.com>
2022-10-19 20:52:48 +0900

    2. TDD, unit test
    - MockMVC, Junit5, assertJ, hamcrest matcher, lombok 설정 추가, 테스트

--- src/main/java/me/ellerymoon/toy/Main.java
+++
@@ -1,8 +0,0 @@
-package me.ellerymoon.toy;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ src/test/java/me/ellerymoon/toy/web/HelloResponseDtoTest.java
@@ -0,0 +1,22 @@
+package me.ellerymoon.toy.web;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class HelloResponseDtoTest {
+
+    @Test
+    public void 롬복_테스트() {
+        // Given
+        String name = "test";
+        int amount = 1000;
+
+        // When
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        // Then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
